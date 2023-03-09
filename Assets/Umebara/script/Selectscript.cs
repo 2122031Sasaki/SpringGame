105b79580158eae61dc33a1c7313e92b7f95861c
Umebara <Nakoichi39@outlook.jp>
2023-03-09 21:50:40 +0900

    画像の追加、スクリプト用ファイルの追加、セレクトスクリプトの追加編集

---
+++ Assets/Umebara/script/Selectscript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Selectscript : MonoBehaviour
+{
+    //”Ô†‚Å‘Î‰‚³‚¹‚é
+    int selectCharaNumber;
+
+    void Start()
+    {
+        //‚Í‚¶‚ß‚Í0
+        selectCharaNumber = 0;
+    }
+
+    public void OnClickA()
+    {
+        //¶‚ğ‘I‘ğ‚µ‚½‚ç1
+        selectCharaNumber = 0;
+    }
+    public void OnClickB()
+    {
+        //“ñ‹U‚ğ‘I‘ğ‚µ‚½‚ç1
+        selectCharaNumber = 1;
+    }
+
+
+    public void OnClickStart()
+    {
+        //”Ô†‚ğ‹L‰¯‚³‚¹‚é
+        PlayerPrefs.SetInt("CHARA_NUMBER", selectCharaNumber);
+        SceneManager.LoadScene("Ustart");
+    }
+}
