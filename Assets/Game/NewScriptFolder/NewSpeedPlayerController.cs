dd5210cd21f0ff2abcdcdb5a6ed698624309491c
Umebara <Nakoichi39@outlook.jp>
2023-03-23 21:35:39 +0900

    フレーム固定(120)

--- Assets/Game/NewScriptFolder/NewSpeedPlayerController.cs
+++ Assets/Game/NewScriptFolder/NewSpeedPlayerController.cs
@@ -32,6 +32,7 @@ public class NewSpeedPlayerController : MonoBehaviour
         LowSpeed = -5.0f;
         Key = 1.0f;
         Rha = true;
+        Application.targetFrameRate = 120;
     }
 
     // Update is called once per frame
