8f3ebee3b361de00a45320e57edd207954c02d8d
2122031-PC\sasaki <s2122031@cco.kanagawa-it.ac.jp>
2023-03-25 18:01:48 +0900

    無敵、障害物にぶつかるとタイムが増える機能の実装

---
+++ Assets/Sasaki/SScripts/TouchObj.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchObj : MonoBehaviour
+{
+    public GameObject manager;
+    public GameObject destroyObj;
+    public bool itemTouched;
+    public bool muteki;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GameObject.Find("GameManager");
+        itemTouched = false;
+        muteki = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Game_star(Clone)")
+        {
+            StartCoroutine("Muteki");
+            destroyObj = other.gameObject;
+            itemTouched = true;
+            Destroy(destroyObj);
+        }
+
+        if (other.gameObject.name == "Game_koori(Clone)")
+        {
+            destroyObj = other.gameObject;
+            if (muteki == false)
+            {
+                manager.GetComponent<timeScript>().Addtime();
+            }
+            Destroy(destroyObj);
+        }
+
+        if (other.gameObject.name == "Game_kamakura(Clone)")
+        {
+            destroyObj = other.gameObject;
+            if (muteki == false)
+            {
+                manager.GetComponent<timeScript>().Addtime();
+            }
+            Destroy(destroyObj);
+        }
+
+        if (other.gameObject.name == "Game_yukikaku(Clone)")
+        {
+            destroyObj = other.gameObject;
+            if (muteki == false)
+            {
+                manager.GetComponent<timeScript>().Addtime();
+            }
+            Destroy(destroyObj);
+        }
+    }
+
+    IEnumerator Muteki()
+    {
+        muteki = true;
+        yield return new WaitForSeconds(10.0f);
+        muteki = false;
+    }
+
+}
