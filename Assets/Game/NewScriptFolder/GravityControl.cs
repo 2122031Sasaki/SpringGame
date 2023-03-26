b9b35bc2c46c4117d3527a1e32613498d79626e1
Umebara <Nakoichi39@outlook.jp>
2023-03-27 03:25:50 +0900

    重力と視点のスクリプトを追加(2)

---
+++ Assets/Game/NewScriptFolder/GravityControl.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityControl : MonoBehaviour
+{
+    bool Rha;
+    bool Rha2;
+    void Start()
+    {
+        Rha = true;
+        Rha2 = true;
+    }
+
+    void FixedUpdate()
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // transformÇéÊìæ
+        Transform myTransform = this.transform;
+
+        // ÉçÅ[ÉJÉãç¿ïWÇäÓèÄÇ…ÅAâÒì]ÇéÊìæ
+        Vector3 localAngle = myTransform.localEulerAngles;
+        // åªç›ÇÃäpìxÇ™180ÇÊÇËëÂÇ´Ç¢èÍçá
+        if (localAngle.x > 180)
+        {
+            // ÉfÉtÉHÉãÉgÇ≈ÇÕäpìxÇÕ0Å`360Ç»ÇÃÇ≈-180Å`180Ç∆Ç»ÇÈÇÊÇ§Ç…ï‚ê≥
+            localAngle.x = localAngle.x - 360;
+        }
+
+
+        if (localAngle.x < -20 && Rha == true)
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        }
+        if (localAngle.x < -30 && Rha == false)
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        }
+        if (localAngle.x < 0 && Rha == true)
+        {
+            localAngle.x += 0.05f;
+        }
+        if (localAngle.x < -20 && Rha == false)
+        {
+            localAngle.x += 0.0025f;
+        }
+
+        if (localAngle.x < -10 && Rha2 == true)
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        }
+        if (localAngle.x < -20 && Rha2 == false)
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        }
+        if (localAngle.x < -10 && Rha2 == false)
+        {
+            localAngle.x += 0.00125f;
+        }
+
+        localAngle.z = 0.0f;
+        myTransform.localEulerAngles = localAngle; // âÒì]äpìxÇê›íË
+
+        if (localAngle.x >= -20 && Rha == false)
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationY;
+        }
+        if (localAngle.x >= -15 && Rha2 == false)
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationY;
+        }
+        if (Rha == false)
+        {
+            Physics.gravity = new Vector3(0, -15, 0);
+        }
+        if (Rha2 == false)
+        {
+            Physics.gravity = new Vector3(0, -18, 0);
+        }
+        if (Rha == true && Rha2 == true)
+        {
+            Physics.gravity = new Vector3(0, -10, 0);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "RotetaController")
+        {
+            Rha = false;
+            Debug.Log("a");
+        }
+        if (other.gameObject.name == "RotetaController2")
+        {
+            Rha2 = false;
+            Debug.Log("c");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "RotetaController")
+        {
+            Rha = true;
+            Debug.Log("b");
+        }
+        if (other.gameObject.name == "RotetaController2")
+        {
+            Rha2 = true;
+            Debug.Log("d");
+        }
+    }
+}
