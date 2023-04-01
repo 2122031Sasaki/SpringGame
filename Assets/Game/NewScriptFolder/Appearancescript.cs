1f4dba93378cded17fed2b1bf214bc8a510ee46a
Umebara <Nakoichi39@outlook.jp>
2023-04-01 14:24:38 +0900

    カメラの位置調整

--- Assets/Game/NewScriptFolder/Appearancescript.cs
+++ Assets/Game/NewScriptFolder/Appearancescript.cs
@@ -12,7 +12,7 @@ public class Appearancescript : MonoBehaviour
     {
         selectCharaNumber= PlayerPrefs.GetInt("CHARA_NUMBER", 0);
         Debug.Log(selectCharaNumber);
-        var obj3 = Instantiate(Camera, new Vector3(1019.0f, 548.0f, 1502.0f), Quaternion.Euler(14, 270.0f, 0));
+        var obj3 = Instantiate(Camera, new Vector3(1019.75f, 548.25f, 1502.0f), Quaternion.Euler(20, 270.0f, 0));
         if (selectCharaNumber == 0)
         {
             var obj = Instantiate(Player_speed, new Vector3(1017.0f, 546f, 1502.0f), Quaternion.Euler(0, 90.0f, 0));
