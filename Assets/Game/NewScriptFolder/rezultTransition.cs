5963af2fb2630727d935100c2bb2b5ddec8ad8dd
Umebara <Nakoichi39@outlook.jp>
2023-04-03 23:14:50 +0900

    たかあやさんおスクリプトを俺のに持ってきた

--- Assets/Game/NewScriptFolder/rezultTransition.cs
+++ Assets/Game/NewScriptFolder/rezultTransition.cs
@@ -7,11 +7,14 @@ public class rezultTransition : MonoBehaviour
 {
     private bool Clear1;
     private bool Clear2;
+    public GameObject GameManeger;
+    float Score;
 
     private void Start()
     {
         Clear1 = false;
         Clear2 = false;
+        Score = PlayerPrefs.GetFloat("SCORE");
     }
 
     private void Update()
@@ -19,11 +22,15 @@ public class rezultTransition : MonoBehaviour
         if (Clear1 == true)
         {
             SceneManager.LoadScene("NewResultScene");
+            Score = GameManeger.GetComponent<timeScript>().scoreTime;
+            PlayerPrefs.SetFloat("SCORE", Score);
         }
 
         if (Clear2 == true)
         {
             SceneManager.LoadScene("NewResultScene2");
+            Score = GameManeger.GetComponent<timeScript>().scoreTime;
+            PlayerPrefs.SetFloat("SCORE", Score);
         }
     }
     private void OnTriggerEnter(Collider other)
